fe0af02d03cb6e44add49853e6c82c6e1c07e71d
Fabián <max_fabian@hotmail.com.ar>
2015-12-12 13:25:46 -0300

    Added functions

--- Taringa.class.php
+++ Taringa.class.php
@@ -64,6 +64,49 @@ class Taringa{
 
 		return (startsWith($result,'0:'))?false:$result;
 	}
+	//Added 12/12/15
+	public function getComments($id){
+		$result= $this->request->getRequest('https://api.taringa.net/shout/comment/view?object_id='.$id);
+		return json_decode($result,true);
+	}
+	public function getShout($id){
+		$result = $this->request->getRequest('https://api.taringa.net/shout/view/'.$id);
+		return json_decode($result,true);
+	}
+	public function sendComment($sId,$body,$cId=null){
+
+		$url= 'https://www.taringa.net/serv/comment/add/'.$sId;
+		if($cId!==null){
+			$url="https://www.taringa.net/serv/comment/addReply/".$sId.".".$cId;
+		}
+
+		$result = $this->request->postRequest($url,array(
+			//'key'=>$this->user_key,
+			'object_type'=>'shout',
+			'body'=>$body
+			));
+		return $result;
+	}
+	public function sendShout($body,$attach=0,$attach_url='',$privacy=1){
+		
+		$result=$this->request->postRequest('https://www.taringa.net/ajax/shout/add',array('key'=>$this->user_key,'body'=>$body,'privacy'=>$privacy,'attachment_type'=>$attach,'attachment'=>$attach_url));
+
+		return (startsWith($result,'0:'))?false:$result;
+	}
+		public function getNotify(){
+		$res=$this->request->getRequest('https://www.taringa.net/clima'); //Hoy es un día gris
+		preg_match_all("/new notifications\('[\w\d]+',\s*(.*?)\)/i", $res, $fndd);
+		return json_decode($fndd[1][0],true);
+	}
+	public function getNewsFeed(){
+		return $result=$this->request->postRequest('https://www.taringa.net/ajax/feed/fetch',array('key' => $this->user_key,'feedName'=>'newsfeed'));
+	}
+	public function voteShout($uid,$ownerid){
+		return $this->request->postRequest('https://www.taringa.net/ajax/shout/vote',array('score'=>'1','key' => $this->user_key,'uuid'=>$uid,'owner'=>$ownerid));
+	}
+	public function getNotifications(){
+		return json_decode($this->request->postRequest('https://www.taringa.net/notificaciones-ajax.php',array('key' => $this->user_key,'action'=>'last','template'=>'false','imageSize'=>'48')),true);
+	}
 }
 function startsWith($haystack, $needle) {
     // search backwards starting from haystack length characters from the end
